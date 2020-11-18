513a8a7197ee852e31df7d0f520f59915326e66b
robertoshimizu <rshimizubr@gmail.com>
2020-11-18 17:20:02 -0300

    feat: :sparkles: add and design SVG icon

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -10,10 +10,14 @@
             {{ property.formattedPrice }}
             <span class="text-gray-600 text-sm">/ wk</span> 
         </div>
-        <div class="mt-4">
-            <span class="text-teal-600 font-semibold">{{ property.rating }}/5 stars</span>
-            <span class="text-gray-600 text-sm"> (based on {{ property.reviewCount }} reviews)</span>
+        <div class="mt-2 flex items-center">
+            <svg  v-for="i in 5" :key="i" :class="i <= property.rating ? 'text-teal-500' : 'text-gray-400'" class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/>
+            </svg>
+            <span class="ml-2 text-gray-600 text-sm"> (based on {{ property.reviewCount }} reviews)</span>
+
         </div>
+        
     </div>
     
 </div>    
